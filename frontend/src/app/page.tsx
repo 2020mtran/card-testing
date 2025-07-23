60eb7d6295f7df9a1dadf18f084944dbe710f685
2020mtran <2020mtran@gmail.com>
2025-07-22 17:00:20 -0700

    Fixed font styling, added labels on the talent tree, and created the first echo stat card.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
             layout="fill"
             className="absolute ml-7 mt-15 scale-125 object-contain object-left"
           />
-        <div className="flex flex-col absolute h-full bg-divider/80 origin-center left-[25.5%] w-500 gap-4">
+        <div className="flex flex-col absolute h-full bg-divider/80 origin-center left-[25.5%] w-500 gap-3">
           <div className="flex flex-row">
             <div className="flex flex-col">
               <div className="flex flex-row items-end gap-3 mt-6 ml-20">
@@ -43,10 +43,10 @@ export default function Home() {
               <div className="flex flex-row items-center gap-1.5">
                 <p className="font-lagu-semibold text-shadow-divider text-shadow-lg ml-20 text-2xl text-white">Lv. 90/90</p>
                 <Image
-                  src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Wuthering-Waves-Spectro.png"
+                  src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Spectro.png"
                   alt="Spectro Symbol"
-                  width={40}
-                  height={40}
+                  width={35}
+                  height={35}
                   className="h-auto"
                   />
               </div>
@@ -55,15 +55,15 @@ export default function Home() {
               <Image
                 src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Blazing-Justice.webp"
                 alt="Blazing Justice"
-                width={80}
-                height={80}
-                className="h-[80px] w-auto"
+                width={70}
+                height={70}
+                className="h-[75px] w-auto"
               />
               <div className="flex flex-col gap-1">
                 <p className="font-lagu-semibold text-shadow-divider text-shadow-lg text-xl text-white leading-none">Blazing Justice</p>
                 <div className="flex flex-row gap-3.5">
                   <p className="font-lagu-semibold text-shadow-divider text-shadow-lg text-lg text-white leading-none">Lv. 90/90</p>
-                  <p className="font-lagu-semibold text-shadow-divider text-shadow-lg text-xl text-white leading-none">R1</p>
+                  <p className="font-lagu-semibold text-shadow-divider text-shadow-lg text-lg text-white leading-none">R1</p>
                 </div>
                 <div className="flex flex-row items-center">
                   <Image 
@@ -86,8 +86,8 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="flex flex-row ml-12">
-            <div className="flex flex-col gap-2 ">
+          <div className="flex flex-row ml-12 items-baseline">
+            <div className="flex flex-col gap-2">
               {leftStats.map(({ icon, label, value }, i) => (
                 <div
                   key={label}
@@ -128,6 +128,10 @@ export default function Home() {
                 <div className="w-11 h-11 rotate-45 border-3 border-black/30 bg-white flex items-center justify-center">
                   <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Zani_Basic_Atk.png" alt="Zani Basic Attack" className="-rotate-45 invert w-9 h-9"></img>
                 </div>
+                <p className="flex flex-col leading-tight text-center mt-3 text-xs">
+                  <span>Lv. 10</span>
+                  <span>Normal</span>
+                </p>
               </div>
               <div className="flex flex-col items-center justify-center">
                 <div className="w-9 h-9 flex justify-center items-center bg-white rounded-full border-3 border-black/30">
@@ -141,6 +145,10 @@ export default function Home() {
                 <div className="w-11 h-11 rotate-45 border-3 border-black/30 bg-white flex items-center justify-center">
                   <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Zani_Resonance_Skill.png" alt="Zani Skill" className="-rotate-45 invert w-9 h-9"></img>
                 </div>
+                <p className="flex flex-col leading-tight text-center mt-3 text-xs">
+                  <span>Lv. 10</span>
+                  <span>Skill</span>
+                </p>
               </div>
               <div className="flex flex-col items-center justify-center">
                 <div className="w-11 h-11 border-3 border-black/30 bg-white flex items-center justify-center">
@@ -158,8 +166,12 @@ export default function Home() {
                 <div className="w-11 h-11 rotate-45 border-3 border-black/30 bg-white flex items-center justify-center">
                   <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Zani_Forte_Circuit.png" alt="Zani Forte Circuit" className="-rotate-45 invert w-9 h-9"></img>
                 </div>
+                <p className="flex flex-col leading-tight text-center mt-3 text-xs">
+                  <span>Lv. 10</span>
+                  <span>Forte</span>
+                </p>
               </div>
-              <div className="flex flex-col items-center justify-center">
+              <div className="flex flex-col items-center justify-center w-[44px]">
                 <div className="w-9 h-9 flex justify-center items-center bg-white rounded-full border-3 border-black/30">
                   <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Attribute_Attack.webp" alt="Atk" className="invert w-6 h-6"></img>
                 </div>
@@ -171,6 +183,10 @@ export default function Home() {
                 <div className="w-11 h-11 rotate-45 border-3 border-black/30 bg-white flex items-center justify-center">
                   <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Zani_Resonance_Liberation.png" alt="Zani Resonance Liberation" className="-rotate-45 invert w-9 h-9"></img>
                 </div>
+                <p className="flex flex-col leading-tight text-center mt-3 text-xs">
+                  <span>Lv. 10</span>
+                  <span>Liberation</span>
+                </p>
               </div>
               <div className="flex flex-col items-center justify-center">
                 <div className="w-9 h-9 flex justify-center items-center bg-white rounded-full border-3 border-black/30">
@@ -184,9 +200,63 @@ export default function Home() {
                 <div className="w-11 h-11 rotate-45 border-3 border-black/30 bg-white flex items-center justify-center">
                   <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Zani_Intro_Skill.png" alt="Zani Basic Attack" className="-rotate-45 invert w-9 h-9"></img>
                 </div>
+                <p className="flex flex-col leading-tight text-center mt-3 text-xs">
+                  <span>Lv. 10</span>
+                  <span>Intro</span>
+                </p>
               </div>
             </div>
           </div>
+          <div className="flex flex-row ml-10">
+              <div className="flex flex-col gap-1 bg-black/30 p-2">
+                <div className="flex flex-row">
+                  <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Capitaneus_Icon.webp" alt="Echo 1" className="w-15 h-15"></img>
+                  <div className="flex flex-col items-end">
+                    <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Eternal_Radiance.webp" alt="Echo 1 Set" className="w-5 h-5"></img>
+                    <p className="text-sm">Spectro DMG</p>
+                    <div className="flex flex-row gap-0.5">
+                      <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Spectro_DMG_Bonus.webp" alt="Echo 1 Main Stat Icon" className="w-5 h-5"></img>
+                      <p className="text-sm">30%</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-row">
+                    <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Attribute_Crit_DMG.webp" alt="Echo 1 Substat Icon" className="w-5 h-5"></img>
+                    <p className="text-sm">Crit DMG</p>
+                  </div>
+                  <p className="text-sm">13.8%</p>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-row">
+                    <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Attribute_Basic_Atk_DMG.png" alt="Echo 1 Substat Icon" className="w-5 h-5"></img>
+                    <p className="text-sm">Basic Atk</p>
+                  </div>
+                  <p className="text-sm">8.6%</p>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-row">
+                    <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Attribute_Crit_Rate.webp" alt="Echo 1 Substat Icon" className="w-5 h-5"></img>
+                    <p className="text-sm">Crit Rate</p>
+                  </div>
+                  <p className="text-sm">7.5%</p>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-row">
+                    <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Attribute_Defense.webp" alt="Echo 1 Substat Icon" className="w-5 h-5"></img>
+                    <p className="text-sm">DEF</p>
+                  </div>
+                  <p className="text-sm">60</p>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-row">
+                    <img src="https://ele2dh89lzgqriuh.public.blob.vercel-storage.com/Icon_Attribute_Attack.webp" alt="Echo 1 Substat Icon" className="w-5 h-5"></img>
+                    <p className="text-sm">ATK</p>
+                  </div>
+                  <p className="text-sm">8.6%</p>
+                </div>
+              </div>            
+            </div>
         </div>
         <div className="absolute top-[-35px] bottom-[-50px] left-[21.5%] w-[65px]">
           <div className="flex flex-col items-center gap-3 h-[150%] bg-divider transform rotate-6 origin-center">
