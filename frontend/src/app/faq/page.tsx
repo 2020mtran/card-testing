a75053f7161575edbb4857824864d496f5f9e7c0
2020mtran <2020mtran@gmail.com>
2025-09-06 12:35:09 -0700

    Fixed build issues

--- frontend/src/app/faq/page.tsx
+++ frontend/src/app/faq/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
-import React, { useState, useEffect, FC, CSSProperties } from 'react';
+import React, { useState } from 'react';
 
 function AccordionItem({ title, children }: { title: string, children: React.ReactNode }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -86,7 +86,7 @@ export default function FaqPage() {
                 The main functionality and purpose of the website has been completed. Due to unfortunate circumstances in my life, I had
                 to drop the project after over 3 months in the making. A few features missing include: localstorage saving, accounts, 
                 and a graph to show who the most popular characters are. Only a portion of the characters / weapons / echoes 
-                fro mthe game are in my site's database as well.
+                fro mthe game are in my site&apos;s database as well.
             </AccordionItem>
             <AccordionItem title="What alternative websites to this could I use?">
                 Below are two alternative websites that inspired this project. Feel free to compare their designs to my site. You may find
